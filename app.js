489933f36e6bd172712f0b185ffe5a65a45c8552
Leonardo Lavigne <lclavigne@gmail.com>
2023-03-27 19:43:16 +0100

    formatacao app.js

--- app.js
+++ app.js
@@ -1,65 +1,67 @@
 fetch('receitas.json')
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Erro ao carregar o arquivo JSON');
-        }
-        return response.json();
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Erro ao carregar o arquivo JSON')
+    }
+    return response.json()
+  })
+  .then(data => {
+    if (!Array.isArray(data)) {
+      throw new Error('O arquivo JSON não contém um array de receitas')
+    }
+
+    const receitasContainer = document.querySelector('.receitas')
+
+    data.forEach(receita => {
+      const receitaDiv = document.createElement('div')
+      receitaDiv.className = 'receita'
+
+      const titulo = document.createElement('h2')
+      titulo.textContent = receita.titulo
+      receitaDiv.appendChild(titulo)
+
+      const ingredientes = document.createElement('ul')
+      receita.ingredientes.forEach(ingrediente => {
+        const li = document.createElement('li')
+        li.textContent = ingrediente
+        ingredientes.appendChild(li)
+      })
+      receitaDiv.appendChild(ingredientes)
+
+      const preparo = document.createElement('p')
+      preparo.textContent = receita.preparo
+      receitaDiv.appendChild(preparo)
+
+      receitasContainer.appendChild(receitaDiv)
     })
-    .then(data => {
-        if (!Array.isArray(data)) {
-            throw new Error('O arquivo JSON não contém um array de receitas');
-        }
+  })
+  .catch(error => {
+    console.error('Erro ao carregar as receitas:', error)
+  })
 
-        const receitasContainer = document.querySelector('.receitas');
+document.getElementById('botao-impressao').addEventListener('click', () => {
+  window.print()
+})
 
-        data.forEach(receita => {
-            const receitaDiv = document.createElement('div');
-            receitaDiv.className = 'receita';
+function exibirReceitas(data) {
+  let receitasHTML = ''
 
-            const titulo = document.createElement('h2');
-            titulo.textContent = receita.titulo;
-            receitaDiv.appendChild(titulo);
-
-            const ingredientes = document.createElement('ul');
-            receita.ingredientes.forEach(ingrediente => {
-                const li = document.createElement('li');
-                li.textContent = ingrediente;
-                ingredientes.appendChild(li);
-            });
-            receitaDiv.appendChild(ingredientes);
-
-            const preparo = document.createElement('p');
-            preparo.textContent = receita.preparo;
-            receitaDiv.appendChild(preparo);
-
-            receitasContainer.appendChild(receitaDiv);
-        });
-    })
-    .catch(error => {
-        console.error('Erro ao carregar as receitas:', error);
-    });
-
-    document.getElementById('botao-impressao').addEventListener('click', () => {
-        window.print();
-    });
-
-    function exibirReceitas(data) {
-        let receitasHTML = '';
-    
-        data.forEach(receita => {
-            receitasHTML += `
+  data.forEach(receita => {
+    receitasHTML += `
                 <div class="receita">
                     <img src="${receita.imagem}" alt="${receita.nome}">
                     <h2>${receita.nome}</h2>
                     <h3>Ingredientes</h3>
                     <ul>
-                        ${receita.ingredientes.map(ingrediente => `<li>${ingrediente}</li>`).join('')}
+                        ${receita.ingredientes
+                          .map(ingrediente => `<li>${ingrediente}</li>`)
+                          .join('')}
                     </ul>
                     <h3>Preparo</h3>
                     <p>${receita.preparo}</p>
                 </div>
-            `;
-        });
-    
-        document.querySelector('.receitas').innerHTML = receitasHTML;
-    }
+            `
+  })
+
+  document.querySelector('.receitas').innerHTML = receitasHTML
+}
